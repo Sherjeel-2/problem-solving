611b80a85f343c64ddab65f1c8244138ff3c3ac4
sherjeelamin <sherjeelamin@gmail.com>
2026-01-18 14:37:52 +0530

    Number is even or odd

---
+++ basics.py
@@ -0,0 +1,14 @@
+#basic input output
+#name = input("Your name: ")
+#age = int(input("Your age: "))
+
+#print(f"Your name is {name} and your age is {age}")
+
+#Check even odd
+
+number = int(input("Input your number: "))
+
+if (number%2) == 0:
+    print(f"{number} is even")
+else:
+    print(f"{number} is odd")
