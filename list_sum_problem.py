a32dac022f9bf0a2dbc7b6d10845e26be54904b3
sherjeelamin <sherjeelamin@gmail.com>
2026-01-21 20:52:39 +0530

    nested loops

--- list_sum_problem.py
+++ list_sum_problem.py
@@ -1,8 +1,33 @@
+#sum the list using range loop method
 list = [1,2,6,7,8,33,12,54,67]
 sum = 0
 print(len(list))
 for i in range (0,len(list)):
-    print(i)
     sum = sum + list[i]
 
 print(f"The sum is {sum}")
+
+#sum the list using while loop
+listTwo = [1,2,6,7,8,33,12,54,67]
+
+j = 0
+sum2 = 0
+while j < len(listTwo):
+    print(j)
+    sum2 = sum2 + listTwo[j]
+    j = j+1
+
+print(f"The sum 2 is {sum2}")
+
+#Nested Loops in Python 
+
+listA = ["red", "yellow", "blue"]
+listB = ["apple", "mango", "cherry"]
+
+# We can use in to iterate list
+for i in listA:
+    for j in listB:
+        print(i,j)
+
+for k in listTwo:
+    print(k)
